50a0dd29190eb89f894718599a1f6220b1382ba2
matheusnicolas <matheuspb8@gmail.com>
2023-04-18 18:44:51 -0700

    first commit

---
+++ msclients/src/test/java/io/github/lofi/msclients/MsclientsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lofi.msclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsclientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
